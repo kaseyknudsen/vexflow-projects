9dbb9816a6ca08d8dcec97968a002f325de9e5ca
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-11-07 23:30:18 -0800

    mapped through note array

--- app/renderNotes/page.js
+++ app/renderNotes/page.js
@@ -1,12 +1,12 @@
 "use client";
 
-import Vex from "vexflow";
+import Vex, { StaveNote } from "vexflow";
 import { useEffect, useRef } from "react";
 import AddNotes from "../components/addNotesToStaff";
 import { noteArray } from "../components/noteData";
 import { addTickables } from "../components/addTickables";
 
-const { Renderer, Stave } = Vex.Flow;
+const { Renderer, Stave, Voice, Formatter } = Vex.Flow;
 
 const renderNotes = () => {
   const notationRef = useRef(null);
@@ -15,11 +15,11 @@ const renderNotes = () => {
     "f/4",
     "g/4",
     "a/4",
-    "b/4",
-    "c/5",
-    "d/5",
-    "e/5",
-    "f/5",
+    // "b/4",
+    // "c/5",
+    // "d/5",
+    // "e/5",
+    // "f/5",
   ];
 
   useEffect(() => {
@@ -30,11 +30,19 @@ const renderNotes = () => {
       );
       renderer.resize(400, 400);
       const context = renderer.getContext();
-      const stave = new Stave(50, 40, 200)
+      const stave = new Stave(50, 40, 300)
         .addClef("treble")
         .addTimeSignature("4/4")
         .setContext(context)
         .draw();
+      const voice = new Voice({ num_beats: 4, beat_value: 4 });
+
+      const newNoteArray = noteArray.map((note, idx) => {
+        return new StaveNote({ keys: [note], duration: "q" });
+      });
+      voice.addTickables(newNoteArray);
+      new Formatter().joinVoices([voice]).format([voice], 250);
+      voice.draw(context, stave);
 
       return () => {
         if (notationRef.current) {
